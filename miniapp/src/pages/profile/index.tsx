e59a55e3cdcc220fb56f4566058110f96a7038f8
bo.wang <bo.wang@eeoa.com>
2025-12-11 13:26:24 +0800

    chore: update version number on profile page from v1.0.0 to v1.3.2 for accurate version tracking

--- miniapp/src/pages/profile/index.tsx
+++ miniapp/src/pages/profile/index.tsx
@@ -341,7 +341,7 @@ export default function Profile() {
 
       {/* 版本号 */}
       <View className='version-info'>
-        <Text className='version-text'>v1.0.0</Text>
+        <Text className='version-text'>v1.3.2</Text>
       </View>
     </View>
   )
