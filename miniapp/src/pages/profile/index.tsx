c33b140182dc8211312abd497da9bc74acf0dc42
bo.wang <bo.wang@eeoa.com>
2025-12-29 15:45:15 +0800

    chore: bump version to v1.9.0

--- miniapp/src/pages/profile/index.tsx
+++ miniapp/src/pages/profile/index.tsx
@@ -1101,7 +1101,7 @@ export default function Profile() {
       )}
 
       <View className='version-info'>
-        <Text className='version-text'>v1.8.2</Text>
+        <Text className='version-text'>v1.9.0</Text>
       </View>
     </View>
   )
